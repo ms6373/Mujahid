71dc8afac0ed6baca2a2bb86a15971b7476bc5d8
MujahidRasool <Malik3682018>
2020-03-22 09:53:49 +0500

    a

---
+++ app/src/main/java/com/example/mylibrary/Mujahid.java
@@ -0,0 +1,15 @@
+package com.example.mylibrary;
+
+public class Mujahid {
+    String Name;
+
+
+    public Mujahid(String name) {
+        Name = name;
+    }
+
+
+    public String getName() {
+        return Name;
+    }
+}
