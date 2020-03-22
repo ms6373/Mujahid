50805ed06d45dbc8b10f08ed288e0a065e680d18
MujahidRasool <Malik3682018>
2020-03-22 11:02:54 +0500

    New

---
+++ app/mujahid/src/main/java/com/digitalspeedometer/speedometer/odometer/mujahid/MujahidMalik.java
@@ -0,0 +1,16 @@
+package com.digitalspeedometer.speedometer.odometer.mujahid;
+
+public class MujahidMalik {
+    String name= "I am Mujahid";
+
+    public MujahidMalik(String name) {
+        this.name = name;
+    }
+
+    public MujahidMalik() {
+    }
+
+    public String getName() {
+        return name;
+    }
+}
